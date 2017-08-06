b46af71bffe8ee75c115a0b8cfc79bda207cf65a
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-08-06 13:22:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-   res.send(createTempalte(articleOne))
+   res.send(createTemplate(articleOne))
 });
 
 app.get('/article-two', function(req,res){
