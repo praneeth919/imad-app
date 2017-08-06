ac6ac693030f3cc507b7877e7d211d23f501e2f2
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-08-06 10:13:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one', function(req,res){
+   res.send('Article one requested andwill be served here'); 
+});
+
+app.get('/article-two', function(req,res){
+   res.send('Article two requested andwill be served here'); 
+});
+
+app.get('/article-three', function(req,res){
+   res.send('Article three requested andwill be served here'); 
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
