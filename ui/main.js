285866791655fea1b8a7212459a350f645e38d47
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-08-07 17:14:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,8 +46,7 @@ button.onclick = function(){
 
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //Create a request
@@ -73,6 +72,8 @@ submit.onclick = function() {
     };
     
     //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://praneeth919.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
     //Make a request to the server and send the name
