7eb2f8c012a83b4aad9981e1a11f2c6490e05bc7
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-08-07 11:38:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML= 'New Value';
 
 // Move the image
 var img = document.getElementById('lol');
+var marginLeft=0;
+function moveRight(){
+    marginLeft = marginLeft +1;
+    img.style.marginLleft = marginLeft + 'px';
+}
 img.onclick= function() {
-    img.style.marginLeft = '100px';
+    img.style.marginLeft = '50px';
 };
