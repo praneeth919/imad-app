9808af0756b61be995630a80c3f072f1c1dc9363
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-08-07 14:40:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,7 @@ img.onclick= function() {
 };
 */
 
+// Counter code
 var button = document.getElementById('counter');
 
 button.onclick = function(){
@@ -41,4 +42,22 @@ button.onclick = function(){
     //Make the request
     request.open('GET', 'http://praneeth919.imad.hasura-app.io/counter',true);
     request.send(null);
+};
+
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //Make a request to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list= '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML= list;
 };
