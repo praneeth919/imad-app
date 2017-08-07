c3a33572513509398f2c36c3c2925d19b8c2434a
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-08-07 13:08:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,15 +19,25 @@ img.onclick= function() {
 */
 
 var button = document.getElementById('counter');
-var counter= 0;
 button.onclick = function(){
     
-    //Make a request to the counter endpoint
+    //Create a request
+    var request = new XMLHttpRequests();
     
     //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequests.DONE){
+            //Take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        
+        }
+    };
     
-    //Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Make the request
+    request.open('GET', 'http:/praneeth919.imad.hasura-app.io/counter',true);
+    requests.send(null);
 };
