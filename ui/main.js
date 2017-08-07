a630f3e052a7e9854715d4a4ef5f06837ad8eb3c
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-08-07 11:31:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+// Change the text of the main text div
+var element= document.getElementById('main-text');
+
+element.innerHTML= 'New Value';
+
+// Move the image
+var img = document.getElementById('lol');
+img.onclick= function() {
+    img.style.marginLeft = '100px';
+};
