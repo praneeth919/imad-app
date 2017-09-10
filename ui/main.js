fa95488dbed889b6ea3dd3e4a8fa9db75f8d533b
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-09-10 20:27:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ function moveRight() {
 img.onclick= function() {
     var interval = setInterval(moveRight,50);
 };
-*/
+
 
 // Counter code
 var button = document.getElementById('counter');
@@ -44,8 +44,9 @@ button.onclick = function(){
     request.send(null);
 };
 
+*/
 
-//Submit name
+//Submit username/password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -57,26 +58,23 @@ submit.onclick = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200){
-                //Capture a list of names and render it as a list
-                var names = request.responseText;
-                names= JSON.parse(names);
-                var list= '';
-                for(var i=0; i<names.length; i++){
-                list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML= list;
+               alert('logged in successfully');
+            } else if(request.status===403) {
+                alert('Username/password is incorrect');
+            } else if(request.status===500){
+                alert('Something went wrong on the server');
+            }
             }
         //Not done yet
         }
     };
     
     //Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://praneeth919.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
-    //Make a request to the server and send the name
-    
-
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://praneeth919.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
