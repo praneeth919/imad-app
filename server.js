d268b60f054bc53744312f184d64e155c5a7c4bd
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-09-10 18:10:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,6 +115,31 @@ app.post('/create-user', function(req,res){
             res.send("User successfully created: " + username);
         }
     });
+});
+
+app.post('/login', function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    pool.query('SELECT * from "user" username = $1',[username], function(err,result){
+        if(err) {
+            res.status(500).send(err.toString());
+        } else{
+            if(result.rows.length ===0){
+                res.send(403).send('username/password is invalid')
+            } else{
+                //Match the password
+                var dbString = result.rows[0].password;
+                var salt= dbString.split('$')[2]
+                var hashedPassword= hash(password, salt); //creating a hash based on the password submitted and the original salt
+                if(hashedPassword === dbString){
+                    res.send('credentials correct');
+                } else{
+                    res.send(403).send('username/password is invalid')
+                }
+        }
+      }    
+    });
     
 });
 
