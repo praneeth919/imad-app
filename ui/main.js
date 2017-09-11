22aebd78684fb8b5dd046bf7145de26fd4844605
praneeth919 <30458257+praneeth919@users.noreply.github.com>
2017-09-11 19:47:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,12 +55,15 @@ submit.onclick = function() {
     
     //Capture the response and store it in a variable
     request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //Take some action
-            if(request.status === 200){
-               alert('logged in successfully');
-            } else if(request.status===403) {
-                alert('Username/password is incorrect');
+      if(request.readyState === XMLHttpRequest.DONE){
+          
+          
+          //Take some action
+          if(request.status === 200){
+              
+          alert('logged in successfully');
+          } else if(request.status===403) {
+            alert('Username/password is incorrect');
             } else if(request.status===500){
                 alert('Something went wrong on the server');
             }
